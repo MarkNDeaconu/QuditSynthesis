4da1180325ef686107c9b44fa2199ebfe904ea15
mark-code <54897881+mark-code@users.noreply.github.com>
2024-08-13 15:38:47 -0500

    bfs synthesis cooking

--- qu5it.py
+++ qu5it.py
@@ -10,16 +10,19 @@ e2 = cyclotomic_element(z5, [0,0,1,0,0])
 e3 = cyclotomic_element(z5, [0,0,0,1,0])
 e4 = cyclotomic_element(z5, [0,0,0,0,1])
 
-test = cyclotomic_element(z5, [0,4,3,0,1])
+
 
 H = (1/math.sqrt(5))*operator(5,5, [[e0,e0,e0,e0,e0], [e0,e1,e2,e3,e4], [e0,e2,e4,e1,e3], [e0,e3,e1,e4,e2], [e0, e4, e3,e2,e1]])
 
 T = operator(5,5, [[e0,n,n,n,n],[n,e1,n,n,n],[n,n,e3,n,n],[n,n,n,e2,n],[n,n,n,n,e4]])
 
-#mat = H*T*H*T*H*H*T*T*H*T*T*H*T*H*T*T*T*H*H*H*T
-mat= H*T*T*H*T*H*T*H*T*T*H*H*T*H*T*H*T*T*H*T*H*T*T*T*H*T*T*T*H*T*T
+I = H*H*H*H
 
-print(mat)
+#mat = H*T*H*T*H*H*T*T*H*T*T*H*T*H*T*T*T*H*H*H*T
+# mat= H*T*T*H*T*H*T*H*T*T*H*H*T*H*T*H*T*T*H*T*H*T*T*T*H*T*T*T*H*T*T
+
+# print(mat)
+# random.seed(10)
 
 def go_stupid(argument, count=0, depth = random.randint(100,200)):
     if count >depth:
@@ -31,8 +34,43 @@ def go_stupid(argument, count=0, depth = random.randint(100,200)):
         return(go_stupid(argument*T, count+1, depth))
 
 
-test = go_stupid(H)
+def synth_search(oper):
+    H_options = [I,H,H*H,H*H*H]
+    T_options = [I,T,T*T,T*T*T,T*T*T*T]
+
+    all_options = [d*c*b*a for a in H_options for b in T_options for c in H_options for d in T_options]
+
+    for option in all_options:
+        new_mat = option * oper
+        if new_mat.sde < oper.sde:
+            return(new_mat)
+
+'''test = H*T*H*T*T*T*H*H*H*T*H*H*T*T*T*H*T*H*T*H*T*T*H*H*H*T
 print(test)
-print(test.sde_profile())
+print(T*T*T*T*H*H*H*test)
+'''
+test = go_stupid(H)
+
+print(test)
+
+print(synth_search(test))
+
+
+# print(test)
+# print(test.sde_profile())
+
+# better = test
+
+# print(better.sde_profile())
+# print(better)
+
+# print(z5.reduced(np.sum((better.matrix)[:, 0]).coefficients,0))
+
 
 #print(test.comp()) 
+#([-25, -8, -26, -1, 0], 0)
+
+# print(H*H*H)
+
+# print(T)
+# print(T*T*T*T)
--- qutrit.py
+++ qutrit.py
@@ -1,5 +1,5 @@
 from datastructures import *
-
+import random
 z3 =  cyclotomic_ring(3,math.sqrt(3))
 n = cyclotomic_element(z3, [0,0,0]) 
 e0 = cyclotomic_element(z3, [1,0,0])
@@ -12,11 +12,27 @@ H = (1/math.sqrt(3))*operator(3,3, [[e0,e0,e0], [e0,e1,e2], [e0,e2,e1]])
 
 T = operator(3,3,[[e1,n,n],[n,e0,n],[n,n,e2]])
 
-print(H)
-print(H*H*H*H)
+# print(H)
+# print(H*H*H*H)
 
-print(H*H)
+# print(H*H)
 
-print(z3.loc_char)
+# print(z3.loc_char)
 
-print( cyclotomic_element(z3, [9,0,0]))
+# print( cyclotomic_element(z3, [9,0,0]))
+
+def go_stupid(argument, count=0, depth = random.randint(100,200)):
+    if count >depth:
+        return(argument)
+    a= random.randint(0,1)
+    if a:
+        return(go_stupid(argument*H, count+1, depth))
+    
+    else:
+        return(go_stupid(argument*T, count+1, depth))
+    
+
+# print(go_stupid(H))
+
+print(H*T*H*T*T*T*H*H*T*H*T*T*T)
+print(T*T*H*T*H*T*T*T*H*H*T*H*T*T*T*T)
